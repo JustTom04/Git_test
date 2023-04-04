7b88820122c126d3c9639a6c1a0bfe29cad2dc39
Imi <Varjas.Imre.vit@gmail.com>
2023-04-04 11:31:12 +0200

    Feature branch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Git is a usefull software");
+        System.out.print("Java is a good programming language");
+
+        System.out.println("This is a new branch");
     }
 }
