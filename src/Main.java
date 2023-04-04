a18c5a22c7efbcdcd3453a25997be12bc0e5e318
Imi <Varjas.Imre.vit@gmail.com>
2023-04-04 11:31:12 +0200

    Java update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Git is a usefull software");
+        System.out.print("Java is a good programming language");
     }
 }
